aa9c3fd8ab2ba05acbce29a118d8ed4d1ca8190b
Ana-Marija M <ana-marija.mitrevska@students.finki.ukim.mk>
2023-08-08 21:50:26 +0200

    "initial view for homepage"

--- ClosetClutter/models.py
+++ ClosetClutter/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class CharityOrg(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    telephone = models.CharField(max_length=20)
+    mail = models.EmailField(max_length=250)
+    location = models.CharField(max_length=200)
