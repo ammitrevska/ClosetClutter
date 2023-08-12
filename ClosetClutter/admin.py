447d3eda0dd41be747c3ab89274d16cc4ac03dab
DGeorgina <dimitrievska1@yahoo.com>
2023-08-12 15:52:46 +0200

    Initial models created

--- ClosetClutter/admin.py
+++ ClosetClutter/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import CharityOrg
+from .models import CharityOrg, Family, Individual, Subscriber, Event, HumanitarianContainer
 
 admin.site.register(CharityOrg)
-
+admin.site.register(Individual)
+admin.site.register(Family)
+admin.site.register(Subscriber)
+admin.site.register(Event)
+admin.site.register(HumanitarianContainer)
 # Register your models here.
+
 
 
