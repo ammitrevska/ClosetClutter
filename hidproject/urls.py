ab435b29ed311a797bd8203d80270d49a980e924
DGeorgina <dimitrievska1@yahoo.com>
2023-08-13 12:02:46 +0200

    Added routing without forms

--- hidproject/urls.py
+++ hidproject/urls.py
@@ -2,15 +2,24 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-from ClosetClutter.views import index, charityOrganization, charityDetails, individual, individualDetails
+from ClosetClutter.views import index, charityOrganization, charityDetails, individualsAndFamilies, entityDetails, \
+    humanitarianContainers, containerCard, getDirections, containerPic, learnAboutUs, events, eventDetails
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('index/', index, name="index"),
     path('charity/', charityOrganization, name="charityOrganization"),
     path("charity/<int:charityOrg_id>/", charityDetails, name="charityDetails"),
-    path('individual/', individual, name="individual"),
-    path("individual/<int:individual_id>/", individualDetails, name="individualDetails"),
+    path('individualsAndFamilies/', individualsAndFamilies, name="individualsAndFamilies"),
+    path("entity/<int:entity_id>/<str:is_family>/", entityDetails, name="entityDetails"),  # FAMILIES AND INDIVIDUALS
+    path("humanitarianContainers/", humanitarianContainers, name="humanitarianContainers"),
+    path("containerCard/<int:container_id>/", containerCard, name="containerCard"),
+    path("getDirections/<int:container_id>/", getDirections, name="getDirections"),
+    path("containerPic/<int:container_id>/", containerPic, name="containerPic"),
+    path("learnAboutUs/", learnAboutUs, name="learnAboutUs"),
+    path("events/", events, name="events"),
+    path("event/<int:event_id>/", eventDetails, name="eventDetails")
+
 ]
 
 if settings.DEBUG:
