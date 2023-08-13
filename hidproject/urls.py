4bda7376992817be2a33001ad47d2f0bed69ee75
Ana-Marija M <ana-marija.mitrevska@students.finki.ukim.mk>
2023-08-13 17:13:11 +0200

    "map added with marker preview"

--- hidproject/urls.py
+++ hidproject/urls.py
@@ -2,13 +2,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-from ClosetClutter.views import index, charityOrganization, charityDetails
+from ClosetClutter.views import index, charityOrganization, charityDetails, containers
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('index/', index, name="index"),
     path('charity/', charityOrganization, name="charityOrganization"),
     path("charity/<int:charityOrg_id>/", charityDetails, name="charityDetails"),
+    path('containers/', containers, name="containers"),
+
 ]
 
 if settings.DEBUG:
